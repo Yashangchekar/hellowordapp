661aadcd7de0c0c1e542e66260014a2104b1a14a
Yashangchekar <95025917+Yashangchekar@users.noreply.github.com>
2024-07-20 10:37:08 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 20 10:32:50 2024
+
+@author: yash
+"""
+# Assignment 1: Hello World Program
+# Objective:
+# Create a simple Python program to print "Hello, World!" to the console.
+# Instructions:
+# Open your preferred Python IDE or text editor.
+# Write a Python script that outputs the text "Hello, World!" to the console.
+# Save the file with a .py extension (e.g., hello_world.py).
+# Run the script to ensure it displays the correct output.
+
+
+print("Hello, World!") #using print function
+a="Hello, World"
+print(a)
